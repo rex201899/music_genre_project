bf136d086b0f6c90740bc286ea647d2618b60733
zhongtai <zr2208@columbia.edu>
2019-05-14 22:09:49 -0400

    build test

---
+++ src/config.py
@@ -0,0 +1,20 @@
+# 10 GENRES USED FOR CLASSIFICATION
+GENRES      = [ 'blues',
+                'classical',
+                'country',
+                'disco',
+                'hiphop',
+                'jazz',
+                'metal',
+                'pop',
+                'reggae',
+                'rock'  ]
+
+# DEFINE PATHS
+DATAPATH        =   '/Users/zhongtairen/Desktop/6820_project/genres/'
+RAW_DATAPATH    =   '/Users/zhongtairen/Desktop/6820_project/data/raw_data.pkl'
+SET_DATAPATH    =   '/Users/zhongtairen/Desktop/6820_project/data/set.pkl'
+MODELPATH       =   '/Users/zhongtairen/Desktop/6820_project/data/net.pt'
+# RAW_DATAPATH    =   '../utils/raw_data.pkl'
+# SET_DATAPATH    =   '../utils/set.pkl'
+# MODELPATH       =   '../utils/net.pt'
